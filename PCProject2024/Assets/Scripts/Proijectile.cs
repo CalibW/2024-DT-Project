52fcc8c92c2c658cc8219c424d1ecdfb53a8911b
CalibW <cjwyness@student.christscollege.com>
2024-07-04 12:42:18 +1200

    Basic Version

---
+++ PCProject2024/Assets/Scripts/Proijectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proijectile : MonoBehaviour
+{
+    public GameObject impactVFX;
+    public float impactidie;
+    private bool collided;
+
+    void OnCollisionEnter(Collision co)
+    {
+        if(co.gameObject.tag !="FireBall" && co.gameObject.tag != "Player" && !collided)
+        {
+            collided = true;
+            var impact = Instantiate (impactVFX, co.contacts[0].point, Quaternion.identity) as GameObject;
+            Destroy (impact, impactidie);
+            Destroy (gameObject);
+        }
+    }
+}
